e2b77ae017088574ca1c1293a81327f17ad786dd
Luiz Otávio <98042825+ILuizOtavio@users.noreply.github.com>
2025-10-24 18:51:59 -0300

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['react-app'],
+  rules: {
+    // Adicione regras específicas se necessário
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
